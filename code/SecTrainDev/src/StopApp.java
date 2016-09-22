d31fff78d622212cc66d8a4e146cc1b7cfd1a6b9
xy19951128alex <1016879293@qq.com>
2016-09-22 11:07:47 +0800

    Add files via upload

---
+++ code/SecTrainDev/src/StopApp.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.*;
+ 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.*;
+import com.amazonaws.services.ec2.model.*;
+
+public class StopApp {
+
+    public terminate(ArrayList<String> instanceIds){
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        // Create the AmazonEC2Client object so we can call various APIs.
+        AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        ec2.setRegion(usWest2);
+        TerminateInstancesRequest TIR=new TerminateInstancesRequest(instanceIds);
+        ec2.terminateInstances(TIR);
+    }
+}
