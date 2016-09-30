8bb71426ee0de98206a66f8508ef82cbba038534
xy19951128alex <1016879293@qq.com>
2016-09-30 13:43:06 +0800

    Update StopApp.java
    Change it to the javabean format

--- code/SecTrainDev/src/StopApp.java
+++ code/SecTrainDev/src/StopApp.java
@@ -12,9 +12,10 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.*;
 import com.amazonaws.services.ec2.model.*;
 
-public class StopApp {
-
-    public terminate(ArrayList<String> instanceIds){
+public class StopApp implements java.io.Serializable{
+    ArrayList<String> instanceIds;
+    AmazonEC2 ec2;
+    public StopApp(){
         AWSCredentials credentials = null;
         try {
             credentials = new ProfileCredentialsProvider().getCredentials();
@@ -26,10 +27,18 @@ public class StopApp {
                     e);
         }
         // Create the AmazonEC2Client object so we can call various APIs.
-        AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+        this.ec2 = new AmazonEC2Client(credentials);
         Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-        ec2.setRegion(usWest2);
-        TerminateInstancesRequest TIR=new TerminateInstancesRequest(instanceIds);
-        ec2.terminateInstances(TIR);
+        this.ec2.setRegion(usWest2);
     }
+    public void setInstanceIds(String instanceId)
+    {
+    	this.instanceIds.add(instanceId);
+    }
+    public void terminate()
+    {
+        TerminateInstancesRequest TIR=new TerminateInstancesRequest(this.instanceIds);
+        this.ec2.terminateInstances(TIR);
+    }
+    
 }
