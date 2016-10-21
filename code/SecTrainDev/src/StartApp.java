2aae99b4415917fdd8f3badde95dc4f669d38ceb
xy19951128alex <1016879293@qq.com>
2016-10-21 14:52:17 +0800

    Update StartApp.java

--- code/SecTrainDev/src/StartApp.java
+++ code/SecTrainDev/src/StartApp.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,76 +33,94 @@ public class StartApp extends HttpServlet
      */
 	ArrayList<String> instanceIds;
 	AmazonEC2 ec2;
-  public StartApp() {
-	  super();
-        // TODO Auto-generated constructor stub
-    }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html;charset=UTF-8");
-		String name =new String(request.getParameter("name").getBytes("ISO8859-1"),"UTF-8");
-		if(name.equals("start"))
+	// 处理 GET 方法请求的方法
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 		{
-			PrintWriter out= response.getWriter();
-			 AWSCredentials credentials = null;
-		        try {
-		            credentials = new ProfileCredentialsProvider().getCredentials();
-		        } catch (Exception e) {
-		            throw new AmazonClientException(
-		                    "Cannot load the credentials from the credential profiles file. " +
-		                            "Please make sure that your credentials file is at the correct " +
-		                            "location (~/.aws/credentials), and is in valid format.",
-		                    e);
-		        }
-		        // Create the AmazonEC2Client object so we can call various APIs.
-		        this.ec2 = new AmazonEC2Client(credentials);
-		        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-		        ec2.setRegion(usWest2);
-		        // Initializes Instance Request
-		        RunInstancesRequest runn = new RunInstancesRequest();
-		        runn.withImageId("ami-e50ed085")
-		                .withInstanceType("t1.micro")
-		                .withMinCount(1)
-		                .withMaxCount(1)
-		                .withKeyName("COMP3100")
-		                .withSecurityGroups("try");
-		        RunInstancesResult run= ec2.runInstances(runn);
-		        //get instance ID
-			    String result =run.getReservation().getInstances().get(0).getInstanceId();
-			    this.instanceIds.add(result);
-			    String DNS = run.getReservation().getInstances().get(0).getPublicDnsName();
-			    out.print("<script>window.open(DNS+'webbank/index.jsp')</script>");
-	    }
-		if(name.equals("stop"))
-		{
-		  AWSCredentials credentials = null;
-		  try {
-		    credentials = new ProfileCredentialsProvider().getCredentials();
-		  } catch (Exception e) {
-		  throw new AmazonClientException(
-		      "Cannot load the credentials from the credential profiles file. " +
-		      "Please make sure that your credentials file is at the correct " +
-		      "location (~/.aws/credentials), and is in valid format.",
-		      e);
-		      }
-		  // Create the AmazonEC2Client object so we can call various APIs.
-		  this.ec2 = new AmazonEC2Client(credentials);
-		  Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-		  this.ec2.setRegion(usWest2);
-		  TerminateInstancesRequest TIR=new TerminateInstancesRequest(this.instanceIds);
-	   this.ec2.terminateInstances(TIR);
-		}
-	}
+			// 设置响应内容类型
+			response.setContentType("text/html;charset=UTF-8");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+			PrintWriter out = response.getWriter();
+			String title = "HTTP Header 请求实例 - 菜鸟教程实例";
+			if(true)
+			{
+				out= response.getWriter();
+				 AWSCredentials credentials = null;
+			        try {
+			            credentials = new ProfileCredentialsProvider().getCredentials();
+			        } catch (Exception e) {
+			            throw new AmazonClientException(
+			                    "Cannot load the credentials from the credential profiles file. " +
+			                            "Please make sure that your credentials file is at the correct " +
+			                            "location (~/.aws/credentials), and is in valid format.",
+			                    e);
+			        }
+			        // Create the AmazonEC2Client object so we can call various APIs.
+			        this.ec2 = new AmazonEC2Client(credentials);
+			        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+			        ec2.setRegion(usWest2);
+			        // Initializes Instance Request
+			        RunInstancesRequest runn = new RunInstancesRequest();
+			        runn.withImageId("ami-e50ed085")
+			                .withInstanceType("t1.micro")
+			                .withMinCount(1)
+			                .withMaxCount(1)
+			                .withKeyName("COMP3100")
+			                .withSecurityGroups("try");
+			        RunInstancesResult run= ec2.runInstances(runn);
+			        String docType =
+			    			"<!DOCTYPE html> \n";
+			    			out.println(docType +
+			    			"<html>\n" +
+			    			"<head><meta charset=\"utf-8\"><title>" + title + "</title></head>\n"+
+			    			"<body bgcolor=\"#f0f0f0\">\n" +
+			    			"<h1 align=\"center\">" + title + "</h1>\n" +
+			    			"<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+			    			"<tr bgcolor=\"#949494\">\n" +
+			    			"<th>Header 名称</th><th>Header 值</th>\n"+
+			    			"</tr>\n");
+
+			    		Enumeration headerNames = request.getHeaderNames();
+
+			    		while(headerNames.hasMoreElements()) {
+			    			String paramName = (String)headerNames.nextElement();
+			    			out.print("<tr><td>" + paramName + "</td>\n");
+			    			String paramValue = request.getHeader(paramName);
+			    			out.println("<td> " + paramValue + "</td></tr>\n");
+			    		}
+			    		out.println("</table>\n");
+			        //get instance ID
+			    		out.println("<h1 align=\"center\">" +"1"+ run.getReservation().getInstances().get(0).getPublicDnsName() 
+			    				+ "</h1>\n"+
+			    		"<h1 align=\"center\">" + run.getReservation().getInstances().get(0).getInstanceId() 
+			    				+ "</h1>\n" +"</body></html>");
+				    //String result =run.getReservation().getInstances().get(0).getInstanceId();
+				    //this.instanceIds.add(result);
+				    //String DNS = run.getReservation().getInstances().get(0).getPublicDnsName();
+				    //out.print("<script>window.open(DNS+'webbank/index.jsp')</script>");
+		    }			
+			if(false)
+			{
+			  AWSCredentials credentials = null;
+			  try {
+			    credentials = new ProfileCredentialsProvider().getCredentials();
+			  } catch (Exception e) {
+			  throw new AmazonClientException(
+			      "Cannot load the credentials from the credential profiles file. " +
+			      "Please make sure that your credentials file is at the correct " +
+			      "location (~/.aws/credentials), and is in valid format.",
+			      e);
+			      }
+			  // Create the AmazonEC2Client object so we can call various APIs.
+			  this.ec2 = new AmazonEC2Client(credentials);
+			  Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+			  this.ec2.setRegion(usWest2);
+			  TerminateInstancesRequest TIR=new TerminateInstancesRequest(this.instanceIds);
+		   this.ec2.terminateInstances(TIR);
+			}
+		}
+		// 处理 POST 方法请求的方法
+		public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doGet(request, response);
+		}
 	
 }
