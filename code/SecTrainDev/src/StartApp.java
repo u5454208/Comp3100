7bcaae4f0ce3f6ba9f7239e8df088484f0241c96
Runze.Liu <u5454208@anu.edu.au>
2016-10-24 16:42:32 +1100

    Add css

--- code/SecTrainDev/src/StartApp.java
+++ code/SecTrainDev/src/StartApp.java
@@ -34,14 +34,29 @@ public class StartApp extends HttpServlet
 	ArrayList<String> instanceIds;
 	AmazonEC2 ec2;
 	// 处理 GET 方法请求的方法
+	public void init()
+	{
+		instanceIds=new ArrayList();
+	}
 		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 		{
 			// 设置响应内容类型
 			response.setContentType("text/html;charset=UTF-8");
-
 			PrintWriter out = response.getWriter();
+			/*String title = "HTTP Header 请求实例 - 菜鸟教程实例";
+			String docType =
+					"<!DOCTYPE html> \n";
+			out.println(docType +
+					"<html>\n" +
+					"<head><meta charset=\"utf-8\"><title>" + title + "</title></head>\n"+
+					"<body bgcolor=\"#f0f0f0\">\n" +
+					"<h1 align=\"center\">" + title + "</h1>\n" +
+					"<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+					"<tr bgcolor=\"#949494\">\n" +
+					"<th>Header 名称</th><th>Header 值</th>\n"+
+					"</tr>\n");
+			*/
 			String name = request.getParameter("name");
-
 			if(name.equals("start"))
 			{
 				out= response.getWriter();
@@ -60,7 +75,7 @@ public class StartApp extends HttpServlet
 			        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
 			        ec2.setRegion(usWest2);
 			        // Initializes Instance Request
-			        RunInstancesRequest runn = new RunInstancesRequest();
+			        /*RunInstancesRequest runn = new RunInstancesRequest();
 			        runn.withImageId("ami-e50ed085")
 			                .withInstanceType("t1.micro")
 			                .withMinCount(1)
@@ -69,7 +84,9 @@ public class StartApp extends HttpServlet
 			                .withSecurityGroups("try");
 			        RunInstancesResult run= ec2.runInstances(runn);
 				    String result =run.getReservation().getInstances().get(0).getInstanceId();
-				    this.instanceIds.add(result);
+				    this.instanceIds.add(result);*/
+					//out.print("<tr><td>" + this.instanceIds.get(0) + "</td>\n");
+					//out.print("<tr><td>" + this.instanceIds.get(1) + "</td>\n");
 				    //String DNS = run.getReservation().getInstances().get(0).getPublicDnsName();
 				    //out.print("<script>window.open(DNS+'webbank/index.jsp')</script>");
 		    }			
@@ -90,8 +107,20 @@ public class StartApp extends HttpServlet
 			  Region usWest2 = Region.getRegion(Regions.US_WEST_2);
 			  this.ec2.setRegion(usWest2);
 			  TerminateInstancesRequest TIR=new TerminateInstancesRequest(this.instanceIds);
-		   this.ec2.terminateInstances(TIR);
+				this.ec2.terminateInstances(TIR);
 			}
+
+
+			/*Enumeration headerNames = request.getHeaderNames();
+
+			while(headerNames.hasMoreElements()) {
+				String paramName = (String)headerNames.nextElement();
+				out.print("<tr><td>" + paramName + "</td>\n");
+				String paramValue = request.getHeader(paramName);
+				out.println("<td> " + paramValue + "</td></tr>\n");
+			}
+
+			out.println("</table>\n"+name+"\n</body></html>");*/
 		}
 		// 处理 POST 方法请求的方法
 		public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
