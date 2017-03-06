14d3b9617665d01e4fa934d598fb9a7bd5e81534
Runze.Liu <u5454208@anu.edu.au>
2017-03-06 20:35:50 +1100

    Add Mysql sign up function

--- code/SecTrainDev/src/MysqlCon.java
+++ code/SecTrainDev/src/MysqlCon.java
@@ -1,20 +1,26 @@
 /**
  * Created by lrz0927 on 6/3/17.
  */
+
 import java.sql.*;
-class MysqlCon{
-    public static void main(String args[]){
+
+class MysqlCon {
+    public static void main(String args[]) {
+        String username = "slbj";
+        String password = "dsb";
+
+        new MysqlCon().sign_up(username, password);
+    }
+
+
+    public void sign_up(String username, String password) {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/account","root","1234567890");
-
-
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from token");
-            while(rs.next())
-                System.out.println(rs.getString(1)+"  "+rs.getString(2));
-            con.close();
+            stmt.executeUpdate("INSERT INTO userinfo VALUES ('"+username+"','"+password+"')");
+
         }catch(Exception e){ System.out.println(e);}
     }
 }
