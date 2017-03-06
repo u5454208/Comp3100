8fb3ebd73431a40c1711503a8a977e7c32d6ddc4
xy19951128alex <1016879293@qq.com>
2017-03-06 22:11:33 +1100

    Update MysqlCon.java

--- code/SecTrainDev/src/MysqlCon.java
+++ code/SecTrainDev/src/MysqlCon.java
@@ -3,21 +3,54 @@
  */
 
 import java.sql.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-class MysqlCon {
-    public static void main(String args[]) {
-        String username = "admin";
-        String password = "password";
+import java.util.Enumeration;
 
-        new MysqlCon().sign_up(username, password);
-        //new MysqlCon().login(username, password);
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.*;
+
+class MysqlCon extends HttpServlet {
+        String method;
+        String username;
+        String password;
+    public void init()
+    {
+        method=new String;
+        username=new String;
+        password=new String;
     }
-
-
-    public void connector() {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String method = request.getParameter("method");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(method.equals("signup"))
+        {
+            out= response.getWriter();
+            new MysqlCon().sign_up(username, password);
+        }
+        if(method.equals("login"))
+        {
+            out= response.getWriter();
+            new MysqlCon().login(username, password);
+        }
     }
-
+    // 处理 POST 方法请求的方法
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
     public void sign_up(String username, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -27,15 +60,15 @@ class MysqlCon {
             ResultSet rs = stmt.executeQuery("SELECT username FROM userinfo WHERE username = '" + username + "'");
             if (!rs.next()){
                 stmt.executeUpdate("INSERT INTO userinfo VALUES ('" + username + "','" + password + "')");
+                response.addHeader("result", "Pass");
             }else {
-                System.out.println("gndy");
+                response.addHeader("result", "gndy");
             }
 
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-
     public void login(String username, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -44,10 +77,11 @@ class MysqlCon {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT password FROM userinfo WHERE username = '" + username + "'");
             while (rs.next()) {
+                System.out.println(rs.getString(1));
                 if (rs.getString(1).equals(password)) {
-                    System.out.println("Pass");
+                    response.addHeader("result", "Pass");
                 } else {
-                    System.out.println("SB");
+                    response.addHeader("result", "SB");
                 }
             }
         } catch (Exception e) {
