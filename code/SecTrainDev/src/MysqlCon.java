24b4e526b97ebaa4dbb34090eb9e0238c4553185
xy19951128alex <1016879293@qq.com>
2017-03-13 21:43:09 +1100

    Update MysqlCon.java

--- code/SecTrainDev/src/MysqlCon.java
+++ code/SecTrainDev/src/MysqlCon.java
@@ -35,7 +35,7 @@ public class MysqlCon extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         String method = request.getParameter("method");
-        if(method.equals("token"))
+        if(method.equals("grade"))
         {
             String token = request.getParameter("token");
             out= response.getWriter();
