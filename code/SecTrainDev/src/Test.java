238343e4a98950e43ff5684cd09ed8c8b0b03550
xy19951128alex <1016879293@qq.com>
2017-03-29 20:26:13 +1100

    Update Test.java

--- code/SecTrainDev/src/Test.java
+++ code/SecTrainDev/src/Test.java
@@ -2,77 +2,56 @@
  * Created by lrz0927 on 10/3/17.
  */
 
-import java.sql.*;
 
-class Test {
-    public static void main(String args[]) {
-        //String username = "admi";
-        //String password = "password";
 
-        //new Test().sign_up(username, password);
-        //new MysqlCon().login(username, password);
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.DockerClient.AttachParameter;
+import com.spotify.docker.client.LogStream;
+import com.spotify.docker.client.exceptions.DockerCertificateException;
 
-        String token = "flag{0ff3nsiVE_cY6eR_0pERAT1oN}";
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.*;
+public class text extends HttpServlet {
 
-        new Test().token(token);
+	String selection;
+public void init()
+{
+    selection=new String();
+}
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+{
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    select	ion = request.getParameter("selection");
+    if(selection.equals("webbank"))
+    {
+    	String questionID= "c0fb4f9a5021";
+        out= response.getWriter();
+        new tmp().selection(questionID, response);
     }
+}
+// 处理 POST 方法请求的方法
+public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request, response);
+}
+	public void selection(String questionID) throws DockerCertificateException {
+		// TODO Auto-generated method stub
+		final DockerClient docker = DefaultDockerClient.fromEnv().build();
+		docker.startContainer(questionID);
+		
+		docker.stopContainer(questionID, 1800);
+		
+	}
 
-
-    public void connector() {
-
-    }
-
-    public void sign_up(String username, String password) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/account", "root", "1234567890");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT username FROM userinfo WHERE username = '" + username + "'");
-            if (!rs.next()){
-                stmt.executeUpdate("INSERT INTO userinfo VALUES ('" + username + "','" + password + "')");
-            }else {
-                System.out.println("gndy");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    public void login(String username, String password) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/account", "root", "1234567890");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT password FROM userinfo WHERE username = '" + username + "'");
-            while (rs.next()) {
-                if (rs.getString(1).equals(password)) {
-                    System.out.println("Pass");
-                } else {
-                    System.out.println("SB");
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    public void token(String token) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/account", "root", "1234567890");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT score FROM token WHERE token = '" + token + "'");
-            if (rs.next()) {
-                System.out.println(rs.getString(1));
-            }else {
-                System.out.println("0");
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
 }
