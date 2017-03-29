12b4115a8b9deefc3ad05eb04679f74a484e57d3
xy19951128alex <1016879293@qq.com>
2017-03-29 21:07:34 +1100

    Update StartApp.java

--- code/SecTrainDev/src/StartApp.java
+++ code/SecTrainDev/src/StartApp.java
@@ -67,7 +67,7 @@ public class StartApp extends HttpServlet
 			        ec2.setRegion(usWest2);
 			        // Initializes Instance Request
 			        RunInstancesRequest runn = new RunInstancesRequest();
-			        runn.withImageId("ami-62cb5e02")
+			        runn.withImageId("ami-eef86c8e")
 			                .withInstanceType("t2.micro")
 			                .withMinCount(1)
 			                .withMaxCount(1)
