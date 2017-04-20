e8a4bf2ac8ec84c6f1df76acb26bd3f0848b0e52
Runze.Liu <u5454208@anu.edu.au>
2017-04-20 16:36:44 +1000

    !!success useing servlet  \ docker ssh

--- code/SecTrainDev/src/Selection.java
+++ code/SecTrainDev/src/Selection.java
@@ -26,43 +26,49 @@ import com.jcraft.jsch.*;
 
 public class Selection extends HttpServlet {
 
-		String keyfile;
+		String keyfile = "/Users/lrz0927/Desktop/Comp3100/resource/COMP3100.pem";
 		public void init()
 		{
-			keyfile= "\\home\\ec2-user\\COMP3100.pem";
+
 		}
 		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 		{
 			// 设置响应内容类型
-				 response.setContentType("text/html;charset=UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
 		    PrintWriter out = response.getWriter();
-		    selection = request.getParameter("selection");
-		    	  if(selection.equals("webbank"))
+		    String select = request.getParameter("selection");
+		    String publicDNS = request.getParameter("publicDNS");
+			System.out.println(select);
+			if(select.equals("webbank"))
 		    {
-		    		  String questionID= "bank";
+				String questionID= "bank";
 		        out= response.getWriter();
-		         			 new Connection().selection(questionID, response);
+				System.out.println("yep");
+				new Selection().selections(questionID, publicDNS);
 		        response.addHeader("publicDNS", request.getParameter("DNS"));
 		    }
 		}
 		public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			doGet(request, response);
 		}	
-		public void selection(String questionID, String PublicDNS){
+		public void selections(String questionID, String PublicDNS){
 		try{
 	      JSch jsch=new JSch();
 	      String host=null;
 	      host = "ec2-user@" + PublicDNS;
 	      String user=host.substring(0, host.indexOf('@'));
+	      System.out.println(host);
 	      host=host.substring(host.indexOf('@')+1);
+	      System.out.println(keyfile);
 	      jsch.addIdentity(keyfile);
+	      System.out.println("yep");
 	      java.util.Properties config = new java.util.Properties(); 
 	      config.put("StrictHostKeyChecking", "no");
-	      
 	      Session session=jsch.getSession(user, host, 22);
+			System.out.println("yep");
 	      session.setConfig(config);
 	      session.connect();
-
+			System.out.println("yep");
 	      Channel channel=session.openChannel("exec");
 	      String command = "docker start " + questionID;
 	      ((ChannelExec)channel).setCommand(command);  
