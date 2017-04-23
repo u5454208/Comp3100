b1c5d500494c62fccd6cf2e2b6d302eaf83a434d
xy19951128alex <1016879293@qq.com>
2017-04-23 22:03:45 +1000

    Update Selection.java

--- code/SecTrainDev/src/Selection.java
+++ code/SecTrainDev/src/Selection.java
@@ -26,26 +26,26 @@ import com.jcraft.jsch.*;
 
 public class Selection extends HttpServlet {
 
-		String keyfile = "/Users/lrz0927/Desktop/Comp3100/resource/COMP3100.pem";
+		String keyfile = "/Users/lrz0927/Desktop/Comp3100/resource/COMP3100.pem"; //the private key file for SSH connection
 		public void init()
 		{
 
 		}
 		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 		{
-			// 设置响应内容类型
+			
 			response.setContentType("text/html;charset=UTF-8");
 		    PrintWriter out = response.getWriter();
-		    String select = request.getParameter("selection");
-		    String publicDNS = request.getParameter("publicDNS");
-			System.out.println(select);
+		    String select = request.getParameter("selection"); //this shows Parameters comes from front end. This one is for the problem the user selected
+		    String publicDNS = request.getParameter("publicDNS");// This is the public DNS of target in Amazon web service EC2 instance, which is our virtual machine
+			System.out.println(select); //for debuging
 			if(select.equals("webbank"))
 		    {
-				String questionID= "bank";
+				String questionID= "bank"; // this is the name of Docker container running in new instance
 		        out= response.getWriter();
-				System.out.println("yep");
+				System.out.println("yep"); // for debuging
 				new Selection().selections(questionID, publicDNS);
-		        response.addHeader("publicDNS", request.getParameter("DNS"));
+		        response.addHeader("publicDNS", request.getParameter("DNS")); // add response header for opening the new window in front end
 		    }
 		}
 		public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -55,32 +55,31 @@ public class Selection extends HttpServlet {
 		try{
 	      JSch jsch=new JSch();
 	      String host=null;
-	      host = "ec2-user@" + PublicDNS;
+	      host = "ec2-user@" + PublicDNS; 
 	      String user=host.substring(0, host.indexOf('@'));
-	      System.out.println(host);
+	      System.out.println(host); //for debug
 	      host=host.substring(host.indexOf('@')+1);
-	      System.out.println(keyfile);
+	      System.out.println(keyfile); // for debug
 	      jsch.addIdentity(keyfile);
-	      System.out.println("yep");
-	      java.util.Properties config = new java.util.Properties(); 
+	      System.out.println("yep"); //for debug
+	      java.util.Properties config = new java.util.Properties(); //avoid the problem of unknown hosts, though it's insecure 
 	      config.put("StrictHostKeyChecking", "no");
 	      Session session=jsch.getSession(user, host, 22);
-			System.out.println("yep");
+			System.out.println("yep");// for debug
 	      session.setConfig(config);
 	      session.connect();
-			System.out.println("yep");
 	      Channel channel=session.openChannel("exec");
-	      String command = "docker start " + questionID;
+	      String command = "docker start " + questionID; //command for start container. It is the same as the "docker start bank" in target terminal
 	      ((ChannelExec)channel).setCommand(command);  
 	      
 	      channel.setInputStream(null);
 
 	      ((ChannelExec)channel).setErrStream(System.err);
 	      channel.connect();
-	      Thread.sleep(60000);
-	      command = "docker stop " + questionID;
+	      Thread.sleep(60000);// time out function. It is used for set exploitation time for user
+	      command = "docker stop " + questionID; 
 		    	((ChannelExec)channel).setCommand(command);  
-		    	channel.connect();
+		    	channel.connect();// stop container
 	      channel.disconnect();
 	      session.disconnect();
 	    }
